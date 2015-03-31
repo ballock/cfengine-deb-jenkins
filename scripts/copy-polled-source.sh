53e93754f7267bf62542957e34a29c98e6c6fd25
root <root@jenkins>
2015-03-31 11:50:21 +0200

    Autocommit, with subrepos and multitar

--- scripts/copy-polled-source.sh
+++ scripts/copy-polled-source.sh
@@ -2,5 +2,5 @@
 
 ATOMIC_JOB_NAME=$( echo $JOB_NAME |cut -d / -f 1 )
 PACKAGE=${ATOMIC_JOB_NAME%-source*}
-cp -av "$JENKINS_HOME"/jobs/"$ATOMIC_JOB_NAME"/workspace/${PACKAGE}* .
+#cp -av "$JENKINS_HOME"/jobs/"$ATOMIC_JOB_NAME"/workspace/${PACKAGE}* .
 cp -av "$JENKINS_HOME"/jobs/"$ATOMIC_JOB_NAME"/workspace/source .
